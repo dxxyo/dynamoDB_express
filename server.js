ea639d3a3487d56388e4653c74fd0d41c90be1af
dxxyo <komangtagnan@gmail.com>
2023-08-24 21:14:01 +0700

    y

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const AWS = require('aws-sdk');
+const app = express();
+
+const client = new AWS.DynamoDB.DocumentClient();
+const tableName = 'iniDataBase';
+
+AWS.config.update({
+    accessKeyId: 'ASIA6GGICQZMY4RW44UF',
+    secretAccessKey: '7/pHH8gRdLWlG1vbbL35iwk3H5DE6Swnq6EnUFbO',
+    region: 'us-east-1' // e.g., 'us-east-1'
+  });
+
+
+// port on which the server listens
+const port = 3000;
+
+app.get("/rows/all", (req, res) => {
+    var params = {
+        TableName: tableName
+    };
+
+    client.scan(params, (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            var items = [];
+            for (var i in data.Items)
+                items.push(data.Items[i]['Name']);
+
+            res.contentType = 'application/json';
+            res.send(items);
+        }
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
+
+
